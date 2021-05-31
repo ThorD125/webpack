cc711f16a0c06c3b9b279fef26f82907e02f2e34
ThorD125 <32074843+ThorD125@users.noreply.github.com>
2021-05-31 20:25:09 +0200

    first commit

---
+++ assets/js/main.js
@@ -0,0 +1,35 @@
+//importing librarys
+import "./../library/importer.js"
+import 'bootstrap';
+
+//importing scss begin
+import "./../scss/_main.scss"
+
+//importing images
+import "./../images/testimage.jpg"
+import "./../images/dogggie.jpg"
+
+//importing videos
+import "./../videos/testvideo.mp4"
+
+//importing audio
+import "./../audio/testaudio.mp3"
+
+//importing fonts
+import "./../fonts/Alba.woff2"
+import "./../fonts/Alba.woff"
+
+
+//rest of the js has to go here
+$(".slideshow").slick({
+    infinite: true,
+    // autoplay: true,
+    dots: true,
+    arrows: true,
+    autoplaySpeed: 4000,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    centerMode: true,
+});
+
+$(".fancybox").fancybox();
